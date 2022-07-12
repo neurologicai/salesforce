b184990a86bf38189d30162c9e084a363a983cf1
gildemberg-santos <gildemberg.santos@gmail.com>
2022-07-12 14:15:39 -0300

    refresh token

--- lib/salesforce/version.rb
+++ lib/salesforce/version.rb
@@ -4,14 +4,4 @@ module Salesforce
   VERSION = '0.1.16'
   API_VERSION = 'v54.0'
   TIMEZONE = 'America/Sao_Paulo'
-
-  @debug = false
-
-  def self.debug=(value)
-    @debug = value
-  end
-
-  def self.debug?
-    @debug
-  end
 end
